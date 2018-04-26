497b0ae29d89af6a187ccacf97c36f6550e52991
Bjørn Pedersen <bjoern_94@hotmail.com>
2018-04-26 15:29:53 +0200

    Finished implementing widgets on CreateUserActivity

--- app/src/main/java/com/example/panda/assignment3/Activities/CreateUserActivity.java
+++ app/src/main/java/com/example/panda/assignment3/Activities/CreateUserActivity.java
@@ -2,10 +2,19 @@ package com.example.panda.assignment3.Activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
 
 import com.example.panda.assignment3.R;
 
 public class CreateUserActivity extends AppCompatActivity {
+
+    private EditText etUsername, etPassword, etRepeatPassword, etBirthday, etHeight, etWeight;
+    private RadioButton rbMan, rbWoman;
+    private Button btCreateUserCancel, btCreateUserCreate;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
