a0c72e2e97ea145d4a76711ea1b73a3fe9a67c48
Jacob Lindvald Pedersen <au520517@uni.au.dk>
2018-04-26 16:13:14 +0200

    CreateUser

--- app/src/main/java/com/example/panda/assignment3/Activities/LoginActivity.java
+++ app/src/main/java/com/example/panda/assignment3/Activities/LoginActivity.java
@@ -1,17 +1,31 @@
 package com.example.panda.assignment3.Activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.panda.assignment3.R;
 
 public class LoginActivity extends AppCompatActivity {
 
+    Button btnToCreate;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        btnToCreate = findViewById(R.id.btntoCreate);
+
+
+        btnToCreate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(LoginActivity.this,CreateUserActivity.class);
+                startActivity(i);
+            }
+        });
         // Something
         // Changes
     }
