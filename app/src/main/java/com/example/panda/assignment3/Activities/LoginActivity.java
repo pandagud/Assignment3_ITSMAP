a7f84baec34f5ec85f514281fc19a7067c31a8b7
Jacob Lindvald Pedersen <au520517@uni.au.dk>
2018-05-03 09:31:26 +0200

    Login with auth Working

--- app/src/main/java/com/example/panda/assignment3/Activities/LoginActivity.java
+++ app/src/main/java/com/example/panda/assignment3/Activities/LoginActivity.java
@@ -1,32 +1,109 @@
 package com.example.panda.assignment3.Activities;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.panda.assignment3.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginActivity extends AppCompatActivity {
-
-    Button btnToCreate;
+    private EditText inputEmail, inputPassword;
+    private FirebaseAuth auth;
+    private Button btnSignup, btnLogin, btnReset;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        //auth = FirebaseAuth.getInstance();
 
-        btnToCreate = findViewById(R.id.btntoCreate);
+        //if (auth.getCurrentUser() != null) {
+        //    startActivity(new Intent(LoginActivity.this, LoginActivity.class));
+        //    finish();
+       // }
+
+        btnSignup = findViewById(R.id.btntoCreate);
+        inputEmail = (EditText) findViewById(R.id.email);
+        inputPassword = (EditText) findViewById(R.id.password);
+        btnLogin = (Button) findViewById(R.id.btn_login);
+
+        auth = FirebaseAuth.getInstance();
 
 
-        btnToCreate.setOnClickListener(new View.OnClickListener() {
+        btnSignup.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent i = new Intent(LoginActivity.this,CreateUserActivity.class);
                 startActivity(i);
             }
         });
-        // Something
-        // Changes
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String email = inputEmail.getText().toString();
+                final String password = inputPassword.getText().toString();
+
+                if (TextUtils.isEmpty(email)) {
+                    Toast.makeText(getApplicationContext(), "Enter email address!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+                if (TextUtils.isEmpty(password)) {
+                    Toast.makeText(getApplicationContext(), "Enter password!", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+
+
+
+                //authenticate user
+                auth.signInWithEmailAndPassword(email, password)
+                        .addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                            @Override
+                            public void onComplete(@NonNull Task<AuthResult> task) {
+
+                                if (!task.isSuccessful()) {
+                                    // there was an error
+                                    if (password.length() < 6) {
+                                        inputPassword.setError(getString(R.string.minimum_password));
+                                    } else {
+                                        Toast.makeText(LoginActivity.this, getString(R.string.auth_failed), Toast.LENGTH_LONG).show();
+                                    }
+                                } else {
+                                    Intent intent = new Intent(LoginActivity.this, InformationActivity.class);
+                                    startActivity(intent);
+                                    finish();
+                                }
+                            }
+                        });
+            }
+    });
+
+}
+    @Override
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = auth.getCurrentUser();
+        updateUI(currentUser);
+    }
+    public void updateUI(FirebaseUser currentUser)
+    {
+        if(currentUser!=null)
+        {
+            Intent intent = new Intent(LoginActivity.this, InformationActivity.class);
+            startActivity(intent);
+        }
+
     }
 }
+
