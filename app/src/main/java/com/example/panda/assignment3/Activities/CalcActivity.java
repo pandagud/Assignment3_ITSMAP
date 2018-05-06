e494323e7c099bd842969f9e0de793b48653cb0d
Bjørn Pedersen <bjoern_94@hotmail.com>
2018-05-06 11:23:04 +0200

    Created the methods in CalcActivity for calculating the calorie intake etc.

--- app/src/main/java/com/example/panda/assignment3/Activities/CalcActivity.java
+++ app/src/main/java/com/example/panda/assignment3/Activities/CalcActivity.java
@@ -1,25 +1,147 @@
 package com.example.panda.assignment3.Activities;
 
+import android.content.Intent;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.TextView;
 
+import com.example.panda.assignment3.Model.UserModel;
 import com.example.panda.assignment3.R;
+
+import java.time.Period;
+import java.util.Date;
+import java.util.Calendar;
 
 public class CalcActivity extends AppCompatActivity {
 
+    private double CARB_INTAKE_PERCENTAGE = 0.5;
+    private double FAT_INTAKE_PERCENTAGE = 0.2;
+    private double PROTEIN_INTAKE_PERCENTAGE = 0.3;
+
+    private double currentCalories = 0;
+
     private TextView twCalCalorieGoal, twCalcCarbs, twCalcFat, twCalcProt, twCalcSteps, twCalcStepsKcal;
     private Button btCalcEditUser, btCalcLogOff;
+    private UserModel currentUser;
+    Bundle intentBundle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calc);
 
+        // Initialization of buttons
+        btCalcEditUser = findViewById(R.id.btCalcEditUser);
+        btCalcLogOff = findViewById(R.id.btCalcLogOff);
 
+        // Initialization of testViews
+        twCalCalorieGoal = findViewById(R.id.twCalcCalorieGoal);
+        twCalcCarbs = findViewById(R.id.twCalcCarbs);
+        twCalcFat = findViewById(R.id.twCalcFat);
+        twCalcProt = findViewById(R.id.twCalcProt);
+        twCalcSteps = findViewById(R.id.twCalcSteps);
+        twCalcStepsKcal = findViewById(R.id.twCalcStepsKCal);
+
+
+        // Get information from intent
+         intentBundle = getIntent().getExtras();
+         if(intentBundle != null){
+             if(intentBundle.isEmpty() == false){
+
+             }
+         }
+
+
+        // onClickListener for button to edit user information
+        btCalcEditUser.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent InfoIntent = new Intent(CalcActivity.this,InformationActivity.class);
+                // InfoIntent.put()
+                finish();
+                startActivity(InfoIntent);
+
+            }
+        });
+
+         // onClickListener for button to log off account and go to home page
+        btCalcLogOff.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent LogOffIntent = new Intent(CalcActivity.this, LoginActivity.class);
+                finish();
+                startActivity(LogOffIntent);
+            }
+        });
+
+        UpdateUI();
     }
+
+    // Used for managing results from intent call StartActivityForResult()
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    // Updates the textview elements in the UI
+    private void UpdateUI(){
+        currentCalories = CalculateKalories(currentUser);
+        if(currentCalories > 0) {
+            twCalCalorieGoal.setText(currentCalories + " kcal");
+            twCalcCarbs.setText(CalculateCarbs(currentCalories) + " g");
+            twCalcFat.setText(CalculateFat(currentCalories) + " g");
+            twCalcProt.setText(CalculateProtein(currentCalories) + " g");
+            twCalcSteps.setText("");
+            twCalcStepsKcal.setText("");
+        }
+    }
+
+    // Calculating the kalories needed
+    private double CalculateKalories(UserModel userModel){
+        if(userModel.getSex() == "Male"){
+            return (10 * userModel.getWeight() + 6.25 * userModel.getHeight() - 5 * CalculateAge(userModel.getBirthday()) + 5);
+        } else if(userModel.getSex() == "Female"){
+            return (10 * userModel.getWeight() + 6.25 * userModel.getHeight() - 5 * CalculateAge(userModel.getBirthday()) / 161);
+        } else {
+            return 0;
+        }
+    }
+
+    // Calculate the age based on the users birthday
+    private int CalculateAge(Date birthDate){
+        /*
+        Date currentDate = Calendar.getInstance().getTime();
+        if ((birthDate != null) && (currentDate != null)) {
+            return Period.between(birthDate, currentDate).getYears();
+        } else {
+            return 0;
+        }
+        */
+        return 23;
+    }
+
+    // Calculate carb intake
+    private double CalculateCarbs(double calories){
+        return (calories * CARB_INTAKE_PERCENTAGE)/4;
+    }
+
+    // Calculate fat intake
+    private double CalculateFat(double calories){
+        return (calories * FAT_INTAKE_PERCENTAGE)/9;
+    }
+
+    // Calculate protein intake
+    private double CalculateProtein(double calories){
+        return (calories * PROTEIN_INTAKE_PERCENTAGE)/4;
+    }
+
+
+
+
+
 }
---
+++ app/src/main/java/com/example/panda/assignment3/Model/UserModel.java
@@ -0,0 +1,84 @@
+package com.example.panda.assignment3.Model;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class UserModel {
+
+    private String sex;
+    private Date birthday;
+    private double height;
+    private double weight;
+    private double activityLevel;
+    private ArrayList<Double> stepsList;
+
+    public UserModel(String sex, Date birthday, double height, double weight, double activityLevel, ArrayList<Double> stepsList) {
+        this.sex = sex;
+        this.birthday = birthday;
+        this.height = height;
+        this.weight = weight;
+        this.activityLevel = activityLevel;
+        this.stepsList = stepsList;
+    }
+
+    public ArrayList<Double> getStepsList() {
+        return stepsList;
+    }
+
+    public void setStepsList(ArrayList<Double> stepsList) {
+        this.stepsList = stepsList;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        if(sex == "Male" || sex == "Female") {
+            this.sex = sex;
+        } else{
+            this.sex = "Male";
+        }
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public void setHeight(double height) {
+        if(height > 0) {
+            this.height = height;
+        } else{
+            this.height = 0;
+        }
+    }
+
+    public double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(double weight) {
+        if(weight > 0) {
+            this.weight = weight;
+        } else {
+            this.weight = 0;
+        }
+    }
+
+    public double getActivityLevel() {
+        return activityLevel;
+    }
+
+    public void setActivityLevel(double activityLevel) {
+        this.activityLevel = activityLevel;
+    }
+
+}
