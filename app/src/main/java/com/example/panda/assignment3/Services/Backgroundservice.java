067e5994f72f47b5058ec107a5a3b78b1b0194a4
Jacob Lindvald Pedersen <au520517@uni.au.dk>
2018-05-17 09:43:18 +0200

    Merge branch 'master' of https://github.com/pandagud/Assignment3_ITSMAP
    # Conflicts:
    #	app/src/main/java/com/example/panda/assignment3/Activities/CalcActivity.java

--- app/src/main/java/com/example/panda/assignment3/Services/Backgroundservice.java
+++ app/src/main/java/com/example/panda/assignment3/Services/Backgroundservice.java
@@ -1,11 +1,8 @@
 package com.example.panda.assignment3.Services;
 
-import android.app.Notification;
 import android.app.NotificationManager;
-import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
-import android.graphics.Color;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -14,7 +11,6 @@ import android.os.Binder;
 import android.os.Handler;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
-import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 public class Backgroundservice extends Service implements SensorEventListener{
@@ -26,7 +22,7 @@ public class Backgroundservice extends Service implements SensorEventListener{
     SensorManager sensorManager;
     Sensor stepCounterSensor;
     Sensor stepDetectorSensor;
-    Intent intent;
+    Intent backgroundintent;
     NotificationManager notificationManager;
 
     int currentStepsDetected;
@@ -53,31 +49,26 @@ public class Backgroundservice extends Service implements SensorEventListener{
 
         serviceStopped = false;
 
-        // --------------------------------------------------------------------------- \\
-        // ___ (3) start handler ___ \\
-        /////if (serviceStopped == false) {
-        // remove any existing callbacks to the handler
+
         handler.removeCallbacks(updateBroadcastData);
-        // call our handler with or without delay.
-        handler.post(updateBroadcastData); // 0 seconds
-        /////}
-        // ________________________________________________________
+
+        handler.post(updateBroadcastData);
+
         return START_STICKY;
     }
     @Override
     public void onCreate() {
         super.onCreate();
 
-        intent = new Intent(BROADCAST_BACKGROUND_SERVICE_RESULT);
+        backgroundintent = new Intent(BROADCAST_BACKGROUND_SERVICE_RESULT);
 
     }
 
     private Runnable updateBroadcastData = new Runnable() {
         public void run() {
-            if (!serviceStopped) { // Only allow the repeating timer while service is running (once service is stopped the flag state will change and the code inside the conditional statement here will not execute).
-                // Call the method that broadcasts the data to the Activity..
+            if (!serviceStopped) {
                 broadcastSensorValue();
-                // Call "handler.postDelayed" again, after a specified delay.
+
                 handler.postDelayed(this, 5000);
             }
         }
@@ -88,18 +79,17 @@ public class Backgroundservice extends Service implements SensorEventListener{
             int countSteps = (int) event.values[0];
 
 
-            // -The efficient way of starting a new step counting sequence.-
-            if (stepCounter == 0) { // If the stepCounter is in its initial value, then...
-                stepCounter = (int) event.values[0]; // Assign the StepCounter Sensor event value to it.
+
+            if (stepCounter == 0) {
+                stepCounter = (int) event.values[0];
             }
-            newStepCounter = countSteps - stepCounter; // By subtracting the stepCounter variable from the Sensor event value - We start a new counting sequence from 0. Where the Sensor event value will increase, and stepCounter value will be only initialised once.
+            newStepCounter = countSteps - stepCounter;
         }
 
-        // STEP_DETECTOR Sensor.
-        // *** Step Detector: When a step event is detect - "event.values[0]" becomes 1. And stays at 1!
+
         if (event.sensor.getType() == Sensor.TYPE_STEP_DETECTOR) {
             int detectSteps = (int) event.values[0];
-            currentStepsDetected += detectSteps; //steps = steps + detectSteps; // This variable will be initialised with the STEP_DETECTOR event value (1), and will be incremented by itself (+1) for as long as steps are detected.
+            currentStepsDetected += detectSteps;
         }
     }
 
@@ -109,14 +99,14 @@ public class Backgroundservice extends Service implements SensorEventListener{
     }
     private void broadcastSensorValue() {
         Log.d("test", "Data to Activity");
-        // add step counter to intent.
-        intent.putExtra("Counted_Step_Int", newStepCounter);
-        intent.putExtra("Counted_Step", String.valueOf(newStepCounter));
-        // add step detector to intent.
-        intent.putExtra("Detected_Step_Int", currentStepsDetected);
-        intent.putExtra("Detected_Step", String.valueOf(currentStepsDetected));
-        // call sendBroadcast with that intent  - which sends a message to whoever is registered to receive it.
-        sendBroadcast(intent);
+
+        backgroundintent.putExtra("Counted_Step_Int", newStepCounter);
+        backgroundintent.putExtra("Counted_Step", String.valueOf(newStepCounter));
+
+        backgroundintent.putExtra("Detected_Step_Int", currentStepsDetected);
+        backgroundintent.putExtra("Detected_Step", String.valueOf(currentStepsDetected));
+
+        sendBroadcast(backgroundintent);
     }
     public class BackGroundBind extends Binder {
         public Backgroundservice getService() {
@@ -138,36 +128,6 @@ public class Backgroundservice extends Service implements SensorEventListener{
         serviceStopped = true;
 
     }
-    private void showNotification() {
-        NotificationCompat.Builder notificationBuilder = new NotificationCompat.Builder(this);
-        notificationBuilder.setContentTitle("Pedometer");
-        notificationBuilder.setContentText("Pedometer session is running in the background.");
-        notificationBuilder.setColor(Color.parseColor("#6600cc"));
-        int colorLED = Color.argb(255, 0, 255, 0);
-        notificationBuilder.setLights(colorLED, 500, 500);
-        // To  make sure that the Notification LED is triggered.
-        notificationBuilder.setPriority(Notification.PRIORITY_HIGH);
-        notificationBuilder.setOngoing(true);
 
-        //Intent resultIntent = new Intent(this, MainActivity.class);
-        PendingIntent resultPendingIntent = PendingIntent.getActivity(this,0,new Intent(),0);
-        notificationBuilder.setContentIntent(resultPendingIntent);
-
-        notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
-
-
-        notificationManager.notify(0, notificationBuilder.build());
-
-    }
-
-/*
- private void BroadcastSteps()
-    {
-        Intent i  = new Intent();
-        i.setAction(BROADCAST_BACKGROUND_SERVICE_RESULT);
-        LocalBroadcastManager.getInstance(this).sendBroadcast(i);
-
-    }
- */
 
 }
